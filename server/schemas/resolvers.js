87a75f69f397604007c0018bd6d0e50f4d5d8e82
powerar <arpower@bell.net>
2021-07-08 12:11:37 -0400

    fix profile page bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v')
+          .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
 
@@ -18,13 +18,13 @@ const resolvers = {
     },
     users: async () => {
       return User.find()
-        .select('-__v')
+        .select('-__v -password')
         .populate('thoughts')
         .populate('friends');
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select('-__v')
+        .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
